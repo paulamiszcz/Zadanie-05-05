8bcdb68366418337661d649cbbefcd9bdeaa0471
Paula <paulamiszcz@wp.pl>
2018-06-22 12:18:14 +0200

    POPRAWKA // Wyszczególnienie liczb parzystych i zsumowanie ich

--- src/Liczby.java
+++ src/Liczby.java
@@ -1,16 +1,17 @@
 public class Liczby {
     public static void main(String[] args){
-        int i = 0;
+
         int suma = 0;
 
-        for(i = 0; i<=100; i++) {
+        for(int i = 0; i<=100; i++) {
             if (i % 2 != 0) {
                 continue;
             }
             System.out.println("Liczba parzysta: " + i);
+            suma += i;
         }
-        suma = suma + i;
-        System.out.println("Suma liczb: " + suma + i);
+       // suma += i;
+        System.out.println("Suma liczb: " + suma );
 
     }
 }
